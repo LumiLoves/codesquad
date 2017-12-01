fc0cc9855808acde087adcd3abe578f8e9866f27
Ahreum Kim <kim.ah.rm@gmail.com>
2017-12-01 16:44:25 +0900

    add test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+
+const obj = (function(value) {
+  function print() {
+    console.log('#', this); // window
+    // console.log(this.value); // undefined
+    console.log('##', this.value); // crong
+  }
+  return {
+    value: value,
+    print: function() {
+      // XXX 여기서 call 사용해보기
+      // print(); 
+      print.call(this);
+    }
+  }
+})('crong');
+obj.print();
+
