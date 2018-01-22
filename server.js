e058a56ef11e2d0ede031a81d2455251f30a2547
Ahreum Kim <kim.ah.rm@gmail.com>
2018-01-22 12:26:38 +0900

    레이아웃수정, flex, UI템플릿 구성

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.use(express.static(__dirname + '/src'));
 app.get('/', function(req, res) {
   res.sendfile('index.html');
 });
+app.get('/template', function(req, res) {
+  res.sendfile('template.html');
+});
+
 
 http.listen(3000, function() {
   console.log('listening on *:3000');
