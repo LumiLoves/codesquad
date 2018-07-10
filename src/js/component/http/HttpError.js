369a9654cbfe954f33eb41aab5d59af2fa9d8b11
Ahreum Kim <kim.ah.rm@gmail.com>
2018-07-10 16:38:16 +0900

    renamed: 폴더계층구조 변경 및 이름변경

---
+++ src/js/component/http/HttpError.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * HttpError
+ */
+
+class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = 'HttpError';
+    this.response = response;
+  }
+}
