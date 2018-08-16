dcbef91ab6f83418bbc2cc4ac2590d23a8f059b9
Ahreum Kim <kim.ah.rm@gmail.com>
2018-08-16 20:40:04 +0900

    modify: (테스트 코드를 통한) helper 수정 (애니메이션을 promise로 개선, 성공 응답코드 범위 수정)

--- src/js/utility/helpers.js
+++ src/js/utility/helpers.js
@@ -4,15 +4,6 @@
 
 import HttpError from './../component/http/HttpError.js';
 
-export function injectScriptDOM(src, afterInjectionFn) {
-  const scriptElem = document.createElement('script');
-  scriptElem.src = src;
-  scriptElem.addEventListener('load', () => {
-    afterInjectionFn && afterInjectionFn();
-  });
-  document.body.appendChild(scriptElem);
-}
-
 export function attachIndexToDom(nodeList, selector) {
   const selectorFlag = selector? 'hasSelector' : 'noSelector';
   const handler = {
@@ -28,9 +19,10 @@ export function attachIndexToDom(nodeList, selector) {
 }
 
 export async function getFetchData({ url }) {
+  const successCodeRange = '2[0-9][0-9]';
   const response = await fetch(url);
-
-  if (response.status === 200) {
+  
+  if (RegExp(successCodeRange).test(response.status)) {
     return await response.json();
   } else {
     throw new HttpError(response);
@@ -38,27 +30,35 @@ export async function getFetchData({ url }) {
 }
 
 export function fadeOutElem(elem, OPACITY_INTERVAL_VALUE = 0.01) {
-  elem.style.opacity = 1;
+  return new Promise((resolve) => {
+    elem.style.opacity = 1;
 
-  function decreaseOpacity() {
-    if ((elem.style.opacity -= OPACITY_INTERVAL_VALUE) <= 0) {
-      elem.style.opacity = 0;
-    } else {
-      requestAnimationFrame(decreaseOpacity);
+    function decreaseOpacity() {
+      if ((elem.style.opacity -= OPACITY_INTERVAL_VALUE) <= 0) {
+        elem.style.opacity = 0;
+        resolve();
+      } else {
+        requestAnimationFrame(decreaseOpacity);
+      }
     }
-  }
-  decreaseOpacity();
+    decreaseOpacity();
+  });
 }
 
 export function fadeInElem(elem, OPACITY_INTERVAL_VALUE = 0.01) {
-  elem.style.opacity = 0;
+  return new Promise((resolve) => {
+    elem.style.opacity = 0;
 
-  function increaseOpacity() {
-    let opacityVal = parseFloat(elem.style.opacity);
-    if (!((opacityVal += OPACITY_INTERVAL_VALUE) >= 1)) {
-      elem.style.opacity = opacityVal;
-      requestAnimationFrame(increaseOpacity);        
+    function increaseOpacity() {
+      let opacityVal = parseFloat(elem.style.opacity);
+      if (!((opacityVal += OPACITY_INTERVAL_VALUE) >= 1)) {
+        elem.style.opacity = opacityVal;
+        requestAnimationFrame(increaseOpacity);        
+      } else {
+        elem.style.opacity = 1;
+        resolve();
+      }
     }
-  }
-  increaseOpacity();
+    increaseOpacity();
+  });
 }
